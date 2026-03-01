ed79959db4a917bd45baf956915dfbbbe354a256
saurabh <saurabhsharma92@gmail.com>
2026-02-28 22:08:40 -0800

    Initial Commit

---
+++ SDTool/SDToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDToolApp.swift
+//  SDTool
+//
+//  Created by Saurabh Sharma on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct SDToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SDToolTests/SDToolTests.swift
@@ -0,0 +1,17 @@
+//
+//  SDToolTests.swift
+//  SDToolTests
+//
+//  Created by Saurabh Sharma on 2/28/26.
+//
+
+import Testing
+@testable import SDTool
+
+struct SDToolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
